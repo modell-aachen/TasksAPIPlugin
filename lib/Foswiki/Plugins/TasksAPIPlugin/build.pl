1fc1f1115e893ea963c91fc8c3c82c3d1a152dcd
Sven Meyer <meyer@modell-aachen.de>
2015-03-02 15:00:00 +0100

    Updated build script

--- lib/Foswiki/Plugins/TasksAPIPlugin/build.pl
+++ lib/Foswiki/Plugins/TasksAPIPlugin/build.pl
@@ -4,6 +4,28 @@ use strict;
 BEGIN { unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ); }
 use Foswiki::Contrib::Build;
 
+sub target_build {
+  my $this = shift;
+  $this->_installDeps();
+}
+
+sub _installDeps {
+  my $this = shift;
+
+  local $| = 1;
+  print "Fetching node dependencies:\n";
+  print $this->sys_action( qw(npm install) ) . "\n";
+  print "Done!\n\n";
+
+  print "Fetching bower dependencies:\n";
+  print $this->sys_action( qw(bower update) ) . "\n";
+  print "Done!\n\n";
+
+  print "Building...\n";
+  print $this->sys_action( qw(grunt build) ) . "\n";
+  print "Done!\n\n";
+}
+
 my $build = Foswiki::Contrib::Build->new('TasksAPIPlugin');
 $build->build( $build->{target} );
 
