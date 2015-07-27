947461f931084c48887e7c52787bf91784322491
thulfaut <thulfaut@modell-aachen.de>
2015-07-27 19:16:52 +0200

    Fixed Bug where Click on overlay did not deselect task and next open would show wrong task

--- pub/System/TasksAPIPlugin/src/js/taskpanel.js
+++ pub/System/TasksAPIPlugin/src/js/taskpanel.js
@@ -5,11 +5,13 @@
     if ( $('#task-panel').length === 0 ) {
       $('<div id="task-panel"><div class="close"></div><div class="content"></div></div>').appendTo('body');
       $('<div id="task-overlay"></div>').appendTo('body');
-      $('#task-overlay').on('click', function() {
-        toggle.call(opts);
-        killCKE();
-      });
+      
     }
+
+    $('#task-overlay').off('click').on('click', function() {  
+        toggleDetails(opts);
+
+      });
 
     $('#task-panel > .close').off('click').on('click', function() {
       toggleDetails(opts);
