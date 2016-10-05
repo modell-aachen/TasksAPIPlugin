56929584480fc6c0dfde5ba160c6bcd9952cb907
Andre Tebart <tebart@modell-aachen.de>
2016-10-05 10:21:33 +0200

    Adds some comments.
    Ticket: none
    
    Release-Note: none

--- lib/Foswiki/Plugins/TasksAPIPlugin.pm
+++ lib/Foswiki/Plugins/TasksAPIPlugin.pm
@@ -251,6 +251,9 @@ sub afterSaveHandler {
         my $res = _query(query => {Context => "$tweb.$ttopic", TopicType => 'task-prototype'});
         return unless defined $res && @{$res->{tasks}};
 
+        # Tasks are sorted such that tasks without parents are copied first.
+        # This ensures that their new id can be set as the parent on copied
+        # child tasks.
         my @stasks = sort { $a->{fields}{Parent} cmp $b->{fields}{Parent} } @{$res->{tasks}};
         my $newTasksMapping = {};
         foreach my $t (@stasks) {
