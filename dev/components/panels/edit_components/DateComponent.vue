6364362ae29e63c635ec2ecb5eda2e78f81a2825
brunner <brunner@modell-aachen.de>
2017-01-09 11:40:01 +0100

    First version of datepicker for edit-panel.
    Ticket: none
    QWE-2878

--- dev/components/panels/edit_components/DateComponent.vue
+++ dev/components/panels/edit_components/DateComponent.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>Please implement me! I am a date!</div>
+    <div class="flatskin-wrapped">
+      <div class="input-group">
+        <input type="text" data-format="dd.mm.yyyy" data-epoch="" name="" data-name="DueDate" ref="datepicker" v-bind:placeholder="placeholder">
+        <span class="input-group-label" style="margin: 0 0 1rem;"><i class="fa fa-calendar calendersymbol" aria-hidden="true"></i></span>
+      </div>
+    </div>
 </template>
 
 <script>
+/* global $ */
 import MetaFieldMixin from '../../../mixins/MetaFieldMixin.vue';
 export default {
-    mixins: [MetaFieldMixin]
+    mixins: [MetaFieldMixin],
+    props: ["placeholder"],
+    mounted: function () {
+		let self = this;
+		let $datepicker = $(this.$refs.datepicker).pickadate();
+		$datepicker.on("open",function() {
+			self.fields[self.fieldName].value = this.get();
+		});
+    }
 };
 </script>
 
