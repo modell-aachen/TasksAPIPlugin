e00fde18a49d7ce73241161932274a7477aacc9f
Andre Tebart <tebart@modell-aachen.de>
2017-01-11 13:59:20 +0100

    Removes linter warnings from date component.
    QWE-2870
    
    Ticket: none
    
    Release-Note(){
    }

--- dev/components/panels/edit_components/DateComponent.vue
+++ dev/components/panels/edit_components/DateComponent.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="flatskin-wrapped">
       <div class="input-group">
-      	<span class="input-group-label"><i class="fa fa-calendar calendersymbol" aria-hidden="true"></i></span>
+        <span class="input-group-label"><i class="fa fa-calendar calendersymbol" aria-hidden="true"></i></span>
         <input type="text" class="input-group-field" data-format="dd.mm.yyyy" data-epoch="" name="" data-name="DueDate" ref="datepicker" v-bind:placeholder="placeholder">
-        
       </div>
     </div>
 </template>
@@ -15,16 +14,15 @@ export default {
     mixins: [MetaFieldMixin],
     props: ["placeholder"],
     mounted: function () {
-		let self = this;
-		let $datepicker = $(this.$refs.datepicker).pickadate({
-			format: 'dd.mm.yyyy',
-			onSet: function(thingSet) {
-				self.fields[self.fieldName].value = Math.floor(thingSet.select/1000);
-				console.log('Date: ', thingSet.select);
-			}
-		}).pickadate('picker');
-		$datepicker.set('select', Math.floor(self.fields[self.fieldName].value*1000));
-	}
+        let self = this;
+        let $datepicker = $(this.$refs.datepicker).pickadate({
+            format: 'dd.mm.yyyy',
+            onSet: function(thingSet) {
+                self.fields[self.fieldName].value = Math.floor(thingSet.select/1000);
+            }
+        }).pickadate('picker');
+        $datepicker.set('select', Math.floor(self.fields[self.fieldName].value*1000));
+    }
 };
 </script>
 
