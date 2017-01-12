81fa3aa1b6185078cf0cf2be9a15983a564c3e5d
brunner <brunner@modell-aachen.de>
2017-01-12 16:40:30 +0100

    Now Datepicker closes with clicking outside.
    Ticket: none
    QWE-2878

--- dev/components/panels/edit_components/DateComponent.vue
+++ dev/components/panels/edit_components/DateComponent.vue
@@ -4,6 +4,8 @@
         <span class="input-group-label"><i class="fa fa-calendar calendersymbol" aria-hidden="true"></i></span>
         <input type="text" class="input-group-field" ref="datepicker" v-bind:placeholder="placeholder">
       </div>
+      <div class="outside-overlay" v-if="isPickerOpen" @click="closePicker">
+      </div>
     </div>
 </template>
 
@@ -12,6 +14,10 @@
 import MetaFieldMixin from '../../../mixins/MetaFieldMixin.vue';
 export default {
     mixins: [MetaFieldMixin],
+    data: function() {
+        return {isPickerOpen: false,
+                datepicker: null};
+    },
     props: ["placeholder"],
     mounted: function () {
         let self = this;
@@ -21,10 +27,23 @@ export default {
             selectMonths: true,
             onSet: function(thingSet) {
                 self.fields[self.fieldName].value = Math.floor(thingSet.select/1000);
+            },
+            onOpen: function() {
+                self.isPickerOpen = true;
+            },
+            onClose: function() {
+                self.isPickerOpen = false;
             }
         }).pickadate('picker');
         if(self.fields[self.fieldName].value)
             $datepicker.set('select', Math.floor(self.fields[self.fieldName].value*1000));
+        this.datepicker = $datepicker;
+
+    },
+    methods: {
+        closePicker: function() {
+            this.datepicker.close();
+        }
     }
 };
 </script>
@@ -49,5 +68,11 @@ export default {
             max-height: 30em;
         }
     }
-
+    .outside-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
 </style>
