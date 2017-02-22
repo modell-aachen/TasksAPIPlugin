6facccdc7e3e5ec6dea4c4170e79391b5154eb9b
Daniel Supplieth <supplieth@modell-aachen.de>
2017-02-22 14:46:49 +0100

    HotFix: require PickADateContrib for tagGrid.
    Ticket: none
    Release-Note: none

--- lib/Foswiki/Plugins/TasksAPIPlugin.pm
+++ lib/Foswiki/Plugins/TasksAPIPlugin.pm
@@ -1923,6 +1923,9 @@ sub tagGrid {
         return "%RED%TASKSGRID: invalid query ($@)%ENDCOLOR%%BR%";
     }
 
+    require Foswiki::Contrib::PickADateContrib;
+    Foswiki::Contrib::PickADateContrib::initDatePicker();
+
     my @jqdeps = (
         "blockui", "select2", "tabpane", "tasksapi", "ui::dialog",
         "jqp::moment", "jqp::tooltipster", "jqp::underscore",
