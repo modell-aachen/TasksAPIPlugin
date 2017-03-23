94ed244d5a85f943ecaaf2d88e24f9d898eca5da
Andre Tebart <tebart@modell-aachen.de>
2017-03-23 11:43:26 +0100

    Removes type filter from default filter config.
    Ticket: none
    
    Release-Note: none
    id:877

--- lib/Foswiki/Plugins/TasksAPIPlugin.pm
+++ lib/Foswiki/Plugins/TasksAPIPlugin.pm
@@ -1890,7 +1890,7 @@ sub tagGrid {
     my $editorTemplate = $params->{editortemplate};
     my $updateurl = $params->{updateurl} || '';
     my $columns = 'created=Created Author,type=$Badge,assigned=AssignedTo,title=Title $AttachCount $ContextLink,due=DueDate,status=$Signal,checkbox=$Checkbox,'. ($params->{columns} || '');
-    my $filters = $params->{filters} || '"Created" range="1" max="true", "Changed" range="1" max="true", Type';
+    my $filters = $params->{filters} || '"Created" range="1" max="true", "Changed" range="1" max="true"';
     my $headers = 'created=Created:Created,type=Type,assigned=Assigned to,title=Title:Title,due=DueDate:Due date,status=Status,checkbox=,'. ($params->{headers} || '');
     my $captionTemplate = $params->{captiontemplate};
     my $filterTemplate = $params->{filtertemplate};
